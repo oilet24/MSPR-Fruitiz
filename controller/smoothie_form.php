40151f78ef0c702c1fe6e5cb1715bf81617b3200
oilet24 <55381507+oilet24@users.noreply.github.com>
2020-07-14 19:48:23 +0200

    Update smoothie_form.php

--- controller/smoothie_form.php
+++ controller/smoothie_form.php
@@ -1,22 +1,43 @@
+
 <?php
 require_once "./model/connexion.php";               
 ?>
-<form action="../controller/smoothie.php" method="POST">
+<form id="formu" action="controller/smoothie.php" method="POST">
+<div id="recipient">
+  <label for="recipient">
+    Choissisez la taille de votre smoothie :
+  </label>
   <?php
    require "form/recipient.php";               
   ?>
-  <br><br><br>
+</div>
+<div id="fruit">
+  <label for="fruit">
+    Choissisez le fruit de votre smoothie :
   <?php
    require "form/fruit.php";               
   ?>
-  <br><br><br>
+</div>
+<div id="jus">
+  <label for="jus">
+    Choissisez le jus de votre smoothie :
+</label>
   <?php
    require "form/jus.php";               
   ?>
-  <br><br><br>
+</div>
+<div id="supplement">
+  <label for="supplement">
+    Choissisez, si vous le souhaitez, un supplément :
+</label>
   <?php
    require "form/supplement.php";               
   ?>
-  <br><br><br>
+</div>
+<div id="bout">
+  <label for="submit">
+    Il ne vous reste plus qu'a envoyer :  
+</labem>
   <input type="submit" name="submit" value="Envoyer" />
+</div>
 </form>
